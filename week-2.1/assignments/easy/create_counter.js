0b52d0472a699778a5b17df43f6369d9f3e00f5b
levihtkas <62203923+levihtkas@users.noreply.github.com>
2024-08-12 22:23:21 +0530

    Create create_counter.js

---
+++ week-2.1/assignments/easy/create_counter.js
@@ -0,0 +1,14 @@
+## Create a counter in JavaScript
+
+We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript
+It should go up as time goes by in intervals of 1 second
+let i=0;
+
+setInterval(counter,1000);
+
+
+
+function counter(){
+  i=i+1;
+  console.log(i)
+}
